03eae37cdfc7af1c1e22a35f4e53156848589a14
Kisora Thomas <kisora.thomas@gmail.com>
2026-07-22 21:01:54 -0600

    chore: sync from template 04ad89b

--- src/views/MenuView.vue
+++ src/views/MenuView.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
+import { computed } from 'vue'
 import { siteConfig } from '../config/site.config'
 import HeroSection from '@apotome/archetype-shared/components/sections/HeroSection.vue'
+import OrderingSection from '@apotome/archetype-shared/components/sections/OrderingSection.vue'
 import MenuSection from '../components/sections/MenuSection.vue'
+import { useSiteContentStore } from '@apotome/archetype-shared/platform/siteContentStore'
+
+const store = useSiteContentStore()
+const showOrdering = computed(() => store.hasAddOn('ordering'))
 </script>
 
 <template>
@@ -13,7 +19,16 @@ import MenuSection from '../components/sections/MenuSection.vue'
     :image="siteConfig.photos.hero.src"
     :image-alt="siteConfig.photos.hero.alt"
   />
+
+  <OrderingSection
+    v-if="showOrdering"
+    eyebrow="Order"
+    title="Order for pickup"
+    intro="Pick your items, choose a pickup time, and we'll have it ready."
+  />
+
   <MenuSection
+    v-else
     :categories="siteConfig.menu.categories"
     :full-menu-url="siteConfig.menu.fullMenuUrl"
   />
